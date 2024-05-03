2d6a9ad17ad89a458db9cdd46e259ec019e4e175
Pritinandaa <153268106+Pritinandaa@users.noreply.github.com>
2024-05-03 22:10:59 +0530

    Update connectionclass.java

--- connectionclass.java
+++ connectionclass.java
@@ -9,7 +9,7 @@ public class connectionclass {
         try
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con= DriverManager.getConnection("jdbc:mysql:///staffattendance","root","root@98");
+            con= DriverManager.getConnection("jdbc:mysql:///imitstaffattedance","root","root@98");
             stm=con.createStatement();
         }
         catch (Exception ex){
